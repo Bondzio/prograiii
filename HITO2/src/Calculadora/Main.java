77449ce28a897041876175716345a5accc3f880a
ROGERXDCV <roger06cv@gmail.com>
2020-03-24 02:33:19 +0100

    interfaces calculadora

--- HITO2/src/Calculadora/Main.java
+++ HITO2/src/Calculadora/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Calculadora Calc = new Calculadora();
         Calc.IMPRIMIR("la suma es : ", Calc.suma(5,5));
+        Calc.IMPRIMIR("\n la resta es : ", Calc.resta(15,5));
+        Calc.IMPRIMIR("\n la multiplicacion es : ", Calc.multiplicacion(2,5));
+        Calc.IMPRIMIR("\n la divicion es : ", Calc.divicion(100,10));
     }
 }
 
